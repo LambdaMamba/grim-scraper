b7eab0f91fbf19c58d385e8b126466ec04c1dfbf
LambdaMamba <57588612+LambdaMamba@users.noreply.github.com>
2023-05-30 16:35:01 +0900

    Updated resource saver and added exception handling
    Save resources in appropriate folders. Added exception handling and URL input checker.

--- grim-scraper.py
+++ grim-scraper.py
@@ -6,54 +6,83 @@ from webdriver_manager.chrome import ChromeDriverManager
 import requests
 from urllib.parse import urlparse
 import os
+from pathlib import Path
+from tldextract import extract
+import validators
+import sys
+from validator_collection import validators, checkers
 
-link = input("Enter the URL: ")
+try:
+    link = input("Enter the URL: ")
+    if checkers.is_url(link) == True:
+        print("Valid URL, continuing...")
+    elif checkers.is_url(link) == False:
+        print("Invalid URL, exiting...")
+        sys.exit()
 
-options = webdriver.ChromeOptions()
-options.headless = False
+    tsd, td, tsu = extract(link)
+    url_domain = td + '.' + tsu
+    options = webdriver.ChromeOptions()
+    options.headless = False
 
-driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
-urls = []
+    driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+    urls = []
 
-driver.get(link)
-time.sleep(5)
+    driver.get(link)
+    time.sleep(5)
 
-print("Saving screenshot of " +link+" as screenshot.png")
+    screenshot_loc = url_domain+"/screenshot.png"
+    Path(url_domain).mkdir(parents=True, exist_ok=True)
 
-driver.save_screenshot("screenshot.png")
+    print("Saving screenshot of " +link+" in " + screenshot_loc)
 
-print("URL HTTP-Status-Code Content-Type")
+    driver.save_screenshot(screenshot_loc)
 
-for request in driver.requests:
-    if request.response:
-        print(
-            request.url,
-            request.response.status_code,
-            request.response.headers['Content-Type']
-        )
-        urls.append(request.url)
+    print("URL HTTP-Status-Code Content-Type")
 
+    for request in driver.requests:
+        if request.response:
+            print(
+                request.url,
+                request.response.status_code,
+                request.response.headers['Content-Type']
+            )
+            urls.append(request.url)
+except Exception as e:
+    print("Error... exiting")
+    sys.exit()
 
-i = 0
 
 print("Saving the resources.....")
+i = 0
+try:
+    for url in urls:
+        driver.get(url)
+        time.sleep(1)
+        src = driver.page_source
+        file_path = urlparse(url).path
+        file_name = os.path.basename(file_path)
+        only_path = file_path.replace(file_name, '')
+        tsd, td, tsu = extract(url)
+        domain = td + '.' + tsu
+        domain_path = domain + only_path
 
-for url in urls:
-    driver.get(url)
-    time.sleep(1)
-    src = driver.page_source
-    file_path = urlparse(url).path
-    file_name = os.path.basename(file_path)
-    try:
-        f = open(file_name, "w")
+        if file_name == "":
+            file_name = str(i)
+            i = i+1
+
+        scrape_dir = url_domain +"/"+ domain_path
+        full_path = scrape_dir + file_name
+
+        Path(scrape_dir).mkdir(parents=True, exist_ok=True)
+        f = open(full_path, "w")
         f.write(src)
         f.close()
-        print("Saved " + url + " in " + file_name)
-    except FileNotFoundError as e:
-        f = open(str(i), "w")
-        f.write(src)
-        f.close()
-        print("Saved " + url + " in " + str(i))
-        i = i+1
-        
+        print("Saved " + url + " in " + full_path)
+
+    print("Saved the resources successfully")
+except Exception as e:
+    print("Error saving the resources... exiting")
+    sys.exit()
+
 driver.close()
