7333c6ebff2a08d6e6fcca07b5f3eabe29cdd40e
LambdaMamba <57588612+LambdaMamba@users.noreply.github.com>
2023-05-31 00:01:47 +0900

    Refactored code. Added options for running headless and silent mode.
    Added the -headless and -log option. Refactored code and updated documentation.

--- README.md
+++ README.md
@@ -1,30 +1,45 @@
 **The Grim Scraper**
-A Selenium Python Scraper for Scraping websites, specifically phishing websites.
 
-Input a URL when prompted.
+A Selenium Python Scraper for Scraping websites, and downloading their resources.
 
-`Enter the URL: https://github.com/login`
+Usage:
+`python3 grim-scraper.py --url https://github.com/login`
 
-The screenshot of the page will be saved in `screenshot.png`.
+First, it will visit `https://github.com/login` using Selenium, and a screenshot of the site will be saved in `github.com/screenshot.png`.
 
-It will output the URL HTTP-Status-Code Content-Type in the following format.
+Second, it will check the HTTP requests and responses, and will save the `URL HTTP-Status-Code Content-Type` information in `github.com/http_responses.csv`.
+
+Third, it will visit all the URLs found in the HTTP requests and responses. The source codes will be saved in their respective folders and file names.
+
+
+Running in headless mode:
+`python3 grim-scraper.py --url https://github.com/login -headless`
+
+
+Output the logs:
+`python3 grim-scraper.py --url https://github.com/login -log`
+
+Example output with `-log` option enabled:
 ```
 URL HTTP-Status-Code Content-Type
-https://accounts.google.com/ListAccounts?gpsia=1&source=ChromiumBrowser&json=standard 200 application/json; charset=utf-8
-https://github.com/login 200 text/html; charset=utf-8
-https://github.githubassets.com/assets/light-0946cdc16f15.css 200 text/css
-https://github.githubassets.com/assets/dark-3946c959759a.css 200 text/css
-https://github.githubassets.com/assets/primer-0e3420bbec16.css 200 text/css
+('https://accounts.google.com/ListAccounts?gpsia=1&source=ChromiumBrowser&json=standard', 200, 'application/json; charset=utf-8')
+('https://github.com/login', 200, 'text/html; charset=utf-8')
+('https://github.githubassets.com/assets/light-0946cdc16f15.css', 200, 'text/css')
+('https://github.githubassets.com/assets/global-0d04dfcdc794.css', 200, 'text/css')
+('https://github.githubassets.com/assets/dark-3946c959759a.css', 200, 'text/css')
+('https://github.githubassets.com/assets/github-c7a3a0ac71d4.css', 200, 'text/css')
+('https://github.githubassets.com/assets/primer-0e3420bbec16.css', 200, 'text/css')
 ...
 ```
-It will then save all the resources.
+
 ```
-Saved https://accounts.google.com/ListAccounts?gpsia=1&source=ChromiumBrowser&json=standard in ListAccounts
-Saved https://github.com/login in login
-Saved https://github.githubassets.com/assets/light-0946cdc16f15.css in light-0946cdc16f15.css
-Saved https://github.githubassets.com/assets/dark-3946c959759a.css in dark-3946c959759a.css
-Saved https://github.githubassets.com/assets/primer-0e3420bbec16.css in primer-0e3420bbec16.css
-Saved https://github.githubassets.com/assets/github-c7a3a0ac71d4.css in github-c7a3a0ac71d4.css
-Saved https://github.githubassets.com/assets/global-0d04dfcdc794.css in global-0d04dfcdc794.css
+Saving the resources...
+Saved https://accounts.google.com/ListAccounts?gpsia=1&source=ChromiumBrowser&json=standard in github.com/google.com/ListAccounts
+Saved https://github.com/login in github.com/github.com/login
+Saved https://github.githubassets.com/assets/light-0946cdc16f15.css in github.com/githubassets.com/assets/light-0946cdc16f15.css
+Saved https://github.githubassets.com/assets/global-0d04dfcdc794.css in github.com/githubassets.com/assets/global-0d04dfcdc794.css
+Saved https://github.githubassets.com/assets/dark-3946c959759a.css in github.com/githubassets.com/assets/dark-3946c959759a.css
+Saved https://github.githubassets.com/assets/github-c7a3a0ac71d4.css in github.com/githubassets.com/assets/github-c7a3a0ac71d4.css
+Saved https://github.githubassets.com/assets/primer-0e3420bbec16.css in github.com/githubassets.com/assets/primer-0e3420bbec16.css
 ...
 ```
--- grim-scraper.py
+++ grim-scraper.py
@@ -11,78 +11,138 @@ from tldextract import extract
 import validators
 import sys
 from validator_collection import validators, checkers
+import csv
+import argparse
 
-try:
-    link = input("Enter the URL: ")
-    if checkers.is_url(link) == True:
-        print("Valid URL, continuing...")
-    elif checkers.is_url(link) == False:
-        print("Invalid URL, exiting...")
-        sys.exit()
 
-    tsd, td, tsu = extract(link)
-    url_domain = td + '.' + tsu
+def extract_root_domain(url):
+    tsd, td, tsu = extract(url)
+    root_domain = td + '.' + tsu
+    return root_domain
+
+
+def initialize_driver(headless_mode):
     options = webdriver.ChromeOptions()
-    options.headless = False
-
+    options.headless = headless_mode
     driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
-    urls = []
+    print("Driver has been initialized")
+    return driver
 
-    driver.get(link)
-    time.sleep(5)
 
-    screenshot_loc = url_domain+"/screenshot.png"
-    Path(url_domain).mkdir(parents=True, exist_ok=True)
-
-    print("Saving screenshot of " +link+" in " + screenshot_loc)
-
+def take_screenshot(driver, url, root_domain):
+    screenshot_loc = root_domain+"/screenshot.png"
+    Path(root_domain).mkdir(parents=True, exist_ok=True)
     driver.save_screenshot(screenshot_loc)
+    print("Saved screenshot of " +url+" in " + screenshot_loc)
 
-    print("URL HTTP-Status-Code Content-Type")
+def http_responses(driver, url, root_domain, logs):
+    urls = []
+    csv_loc = root_domain+"/http_responses.csv"
+    f = open(csv_loc, "w")
+    writer = csv.writer(f)
+    header = ["URL", "HTTP-Status-Code", "Content-Type"]
+    writer.writerow(header)
+    if logs:
+        print("URL HTTP-Status-Code Content-Type")
 
     for request in driver.requests:
         if request.response:
-            print(
-                request.url,
-                request.response.status_code,
-                request.response.headers['Content-Type']
-            )
+            responses = request.url, request.response.status_code, request.response.headers['Content-Type']
+            if logs:
+                print(responses)
+            writer.writerow(responses)
             urls.append(request.url)
-except Exception as e:
-    print("Error... exiting")
-    sys.exit()
+    f.close()
+    print("Saved HTTP responses info in " + csv_loc)
+    return urls
+
+def save_to_csv(url, status_code, content_type):
+    header = ["URL", "HTTP-Status-Code", "Content-Type"]
+    data = [url, status_code]
 
 
-print("Saving the resources.....")
-i = 0
-try:
-    for url in urls:
+def reap(driver, urls, url_root_domain, logs):
+    print("Saving the resources...")
+    i = 0
+    try:
+        for url in urls:
+            driver.get(url)
+            time.sleep(1)
+            src = driver.page_source
+            file_path = urlparse(url).path
+            file_name = os.path.basename(file_path)
+            only_path = file_path.replace(file_name, '')
+            domain = extract_root_domain(url)
+            domain_path = domain + only_path
+
+            if file_name == "":
+                file_name = str(i)
+                i = i+1
+
+            scrape_dir = url_root_domain +"/"+ domain_path
+            full_path = scrape_dir + file_name
+
+            Path(scrape_dir).mkdir(parents=True, exist_ok=True)
+            f = open(full_path, "w")
+            f.write(src)
+            f.close()
+            if logs:
+                print("Saved " + url + " in " + full_path)
+
+        print("Saved the resources successfully in "+ url_root_domain)
+    except Exception as e:
+        print("Error saving the resources... exiting")
+        sys.exit()
+
+
+
+def main():
+
+    try:
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--url', required=True)
+        parser.add_argument('-headless', action='store_true', help="Run in headless mode")
+        parser.add_argument('-log', action='store_true', help="Output logs")
+        args = parser.parse_args()
+        url = args.url
+        if args.headless:
+            headless_mode = True
+            print("Will run in headless mode...")
+        else:
+            headless_mode = False
+            print("Will run in non-headless mode...")
+
+        if args.log:
+            logs = True
+            print("Will output logs...")
+        else:
+            logs = False
+            print("Will not output logs...")
+
+        if checkers.is_url(url) == True:
+            print("Valid URL, continuing...")
+        elif checkers.is_url(url) == False:
+            print("Invalid URL, exiting...")
+            sys.exit()
+
+        root_domain = extract_root_domain(url)
+        urls = []
+
+        driver = initialize_driver(headless_mode)
         driver.get(url)
-        time.sleep(1)
-        src = driver.page_source
-        file_path = urlparse(url).path
-        file_name = os.path.basename(file_path)
-        only_path = file_path.replace(file_name, '')
-        tsd, td, tsu = extract(url)
-        domain = td + '.' + tsu
-        domain_path = domain + only_path
+        time.sleep(5)
 
-        if file_name == "":
-            file_name = str(i)
-            i = i+1
+        take_screenshot(driver, url, root_domain)
 
-        scrape_dir = url_domain +"/"+ domain_path
-        full_path = scrape_dir + file_name
+        urls = http_responses(driver, url, root_domain, logs)
 
-        Path(scrape_dir).mkdir(parents=True, exist_ok=True)
-        f = open(full_path, "w")
-        f.write(src)
-        f.close()
-        print("Saved " + url + " in " + full_path)
+        reap(driver, urls, root_domain, logs)
 
-    print("Saved the resources successfully")
-except Exception as e:
-    print("Error saving the resources... exiting")
-    sys.exit()
+    except Exception as e:
+        print("Error... exiting")
+        sys.exit()
 
-driver.close()
+    driver.close()
+
+
+main()
