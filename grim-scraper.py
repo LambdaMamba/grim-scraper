942b79caa75a90ab388971993e1bc32fd5d29849
LambdaMamba <57588612+LambdaMamba@users.noreply.github.com>
2023-06-01 15:33:12 +0900

    Include subdomain in folder name, handle exceptions
    Include subdomain in the folder name. Also catch the File Exists Error, which occurs when attempting to create a directory with the same name as an existing file.

--- grim-scraper.py
+++ grim-scraper.py
@@ -18,8 +18,11 @@ import argparse
 
 def extract_root_domain(url):
     tsd, td, tsu = extract(url)
-    root_domain = td + '.' + tsu
-    return root_domain
+    if tsd == "":
+        top = td + '.' + tsu
+    else:
+        top = tsd + '.' + td + '.' + tsu
+    return top
 
 
 def initialize_driver(headless_mode):
@@ -35,6 +38,19 @@ def take_screenshot(driver, url, root_domain):
     Path(root_domain).mkdir(parents=True, exist_ok=True)
     driver.save_screenshot(screenshot_loc)
     print("Saved screenshot of " +url+" in " + screenshot_loc)
+
+
+def check_duplicate(urls, url):
+    duplciate = 0
+    if url[-1]=="/":
+        url = url[:-1]
+    
+    if url in urls:
+        duplicate = True
+    else:
+        duplicate = False
+    return duplicate
+    
 
 def http_responses(driver, url, root_domain, logs):
     urls_filetype = {}
@@ -53,8 +69,13 @@ def http_responses(driver, url, root_domain, logs):
             if logs:
                 print(responses)
             writer.writerow(responses)
+
+            duplicate = check_duplicate(urls, request.url)
+            if duplicate:
+                continue
             urls.append(request.url)
             urls_filetype[request.url] = request.response.headers['Content-Type']
+    
     f.close()
     print("Saved HTTP request/responses info in " + csv_loc)
     return urls, urls_filetype
@@ -66,9 +87,11 @@ def save_to_csv(url, status_code, content_type):
 
 def reap(driver, urls, url_root_domain, logs, all_resource, urls_filetype, filetype):
     print("Saving the resources...")
-    i = 0
     try:
         for url in urls:
+            i=0
+            j=0
+
             domain = extract_root_domain(url)
             filetype_str = str(urls_filetype[url])
             if ((not all_resource) and (domain != url_root_domain)):
@@ -87,33 +110,54 @@ def reap(driver, urls, url_root_domain, logs, all_resource, urls_filetype, filet
                 file_name = "index"
 
             scrape_dir = url_root_domain +"/"+ domain_path
-            full_path = scrape_dir + file_name
-            try:
-                Path(scrape_dir).mkdir(parents=True, exist_ok=True)
-                f = open(full_path, "w")
-                f.write(src)
-                f.close()
-                if logs:
-                    print("Saved " + url + " in " + full_path)
-            except OSError as e:
-                short = file_name[0:10]+"_"+file_name[-10:]
-                scrape_dir = url_root_domain +"/"+ domain_path
-                full_path = scrape_dir + short
-                Path(scrape_dir).mkdir(parents=True, exist_ok=True)
-                f = open(full_path, "w")
-                f.write(src)
-                f.close()
-                if logs:
-                    print("File name too long, changed name to ", short)
-                    print("Saved " + url + " in " + full_path)
-        print("Saved the resources successfully in "+ url_root_domain)
 
+
+            
+
+            if len(file_name) > 250:
+                file_name = file_name[0:10]+"_"+file_name[-10:]
+                if logs:
+                    print("File name too long, changed name to "+ file_name)
+            
+
+            #Cannot make dir if a same filename exists, so rename the directory to something different
+            
+            if not os.path.isdir(scrape_dir):
+                try:
+                    Path(scrape_dir).mkdir(parents=True, exist_ok=True)
+                except OSError as e:
+                    temp = scrape_dir[:-1]
+                    scrape_dir = temp +"_"+str(j)+"/"
+                    Path(scrape_dir).mkdir(parents=True, exist_ok=True)
+                if logs:
+                    print("Made directory "+scrape_dir)
+                time.sleep(0.5)
+            else:
+                if logs:
+                    print(scrape_dir + " directory exists, continuing in that directory...")
+
+            full_path = scrape_dir + file_name
+
+            if os.path.isfile(full_path):
+                new_path = scrape_dir + str(i)+"_"+file_name
+                i=i+1
+                if logs:
+                    print(full_path + " exists, renaming to " +new_path)
+                full_path = new_path
+
+
+            f = open(full_path, "w")
+            f.write(src)
+            f.close()
+            if logs:
+                print("Saved " + url + " in " + full_path)
+            
     except Exception as e:
         print(e)
-        print("Error saving the resources... exiting")
+        print("Error saving the resources... exiting...")
         sys.exit()
 
-
+    print("Saved the resources successfully in "+ url_root_domain)
 
 def main():
 
