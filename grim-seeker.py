27fd7d2e48505308b1ffe0a8d642bf976ae2c0d0
LambdaMamba <57588612+LambdaMamba@users.noreply.github.com>
2023-06-06 00:31:00 +0900

    Made adjustments to regex search mechanism
    Will first search strings inside double/single quote, then will match regex.

--- grim-seeker.py
+++ grim-seeker.py
@@ -13,71 +13,76 @@ def href(file_name, page_string):
         links = link.get('href')
         print(links)
 
-def php(file_name, page_string):
-    php = re.findall(r'(?i)"\S+\.php"', page_string)
-    php2 = re.findall(r"(?i)'\S+\.php'", page_string)
+def doublequote(file_name, page_string):
+    dq_list = []
+    dq = re.findall(r'(?i)"\S+"', page_string)
 
-    if php:
-        print("PHP endpoint found inside " +file_name+":")
-        for url in php:
-            print(url.strip('"'))
+    if dq:
+        for word in dq:
+            dq_list.append(word.strip('"'))
+    return dq_list
 
-    if php2:
-        print("PHP endpoint found inside " +file_name+":")
-        for url in php2:
-            print(url.strip("'"))
+def singlequote(file_name, page_string):
+    sq_list = []
+    sq = re.findall(r"(?i)'\S+'", page_string)
 
+    if sq:
+        for word in sq:
+            sq_list.append(word.strip("'"))
+    return sq_list
 
-def apk(file_name, page_string):
-    apk = re.findall(r'(?i)"\S+\.apk"', page_string)
-    apk2 = re.findall(r"(?i)'\S+\.apk'", page_string)
+def php(file_name, page_string, words):
+    if words:
+        for word in words:
+            if re.match(r'(?i)^\S+\.php$', word):
+                print("PHP endpoint found inside " +file_name+": " + word)
+    
 
-    if apk:
-        print("APK found inside " +file_name+":")
-        for url in apk:
-            print(url.strip('"'))
+def apk(file_name, page_string, words):
+    if words:
+        for word in words:
+            if re.match(r'(?i)^\S+\.apk$', word):
+                print("APK found inside " +file_name+": " + word)
 
-    if apk2:
-        print("APK found inside " +file_name+":")
-        for url in apk:
-            print(url.strip("'"))
-
-def b64(file_name, page_string):
+def b64(file_name, page_string, words):
     #Use regex to search for Base64 encoded strings
-    b_64 = re.findall(r'["](?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{4})["]', page_string)
-    b_64_2 = re.findall(r"['](?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{4})[']", page_string)
-
-    if b_64:
-        for word in b_64:
+    if words:
+        for word in words:
             if len(word) > 10:
-                stripped = word.strip('"')
-                print("Found a possible Base 64 string: "+ stripped)
-                dec_64 = base64.b64decode(stripped)
-                decoded = str(dec_64).strip("b")
-                decoded = decoded.strip("'")
-                decoded = decoded.strip('"')
-                print("Decoded from Base64: "+ decoded)
-    if b_64_2:
-        for word in b_64_2:
-            if len(word) > 10:
-                stripped = word.strip("'")
-                print("Found a possible Base 64 string: "+ stripped)
-                dec_64 = base64.b64decode(stripped)
-                decoded = str(dec_64).strip("b")
-                decoded = decoded.strip("'")
-                decoded = decoded.strip('"')
-                print("Decoded from Base64: "+ decoded)
+                if re.match(r'^(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?$', word):
+                    res = verify_b64(word)
+                    dec_64 = base64.b64decode(word)
+                    decoded = str(dec_64).strip("b")
+                    decoded = decoded.strip("'")
+                    decoded = decoded.strip('"')
+                    res = verify_b64(decoded)
+                    if res:
+                        print("Found a possible Base 64 string: "+ word)
+                        print("Decoded from Base64: "+ decoded)
 
+def verify_b64(decoded):
+    #Verify if base64 encoded, by checking if decoded word contains "\x"
+    if "\\x" in decoded:
+        return False
+    else:
+        return True
 
 def seek(file_name):
     try:
         #print("********* Running grim-seeker *********")
+        dq_list = []
+        sq_list = []
+        words = []
         html_page = open(file_name, 'r')
         page_string = html_page.read()
+        #Search for words enclosed in single or double quotes
+        dq_list = doublequote(file_name, page_string)
+        sq_list = singlequote(file_name, page_string)
+        words = dq_list + sq_list
         href(file_name, page_string)
-        php(file_name, page_string)
-        apk(file_name, page_string)
-        b64(file_name, page_string)
+        php(file_name, page_string, words)
+        apk(file_name, page_string, words)
+        b64(file_name, page_string, words)
 
     except Exception as e:
         print(e)
